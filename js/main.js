e13d8a3a4a57d60fbb73a8a38774ddbb8f7f7a41
vlad-duhota <vladarduino@gmail.com>
2022-10-02 11:03:13 +0300

    beauty hovers sliders

--- js/main.js
+++ js/main.js
@@ -35,6 +35,10 @@ const swiper = new Swiper('.hero-swiper', {
     fadeEffect: {
       crossFade: true
     },
+    speed: 1000,
+    autoplay: {
+      delay: 6500,
+    },
 }
 );
 
@@ -42,6 +46,7 @@ const swiper2 = new Swiper('.category-swiper', {
     loop: true,
     slidesPerView: 3,
     spaceBetween: 40,
+    speed: 600,
     navigation: {
         nextEl: '.category__navigation .nagination__next-btn',
         prevEl: '.category__navigation .nagination__prev-btn',
@@ -61,6 +66,7 @@ const swiper3 = new Swiper('.news-swiper', {
     loop: true,
     slidesPerView: 3,
     spaceBetween: 40,
+    speed: 600,
     navigation: {
         nextEl: '.news__navigation .nagination__next-btn',
         prevEl: '.news__navigation .nagination__prev-btn',
@@ -79,6 +85,7 @@ const swiper4 = new Swiper('.deal-swiper', {
     loop: true,
     slidesPerView: 2,
     spaceBetween: 60,
+    speed: 600,
     navigation: {
         nextEl: '.deal__navigation .nagination__next-btn',
         prevEl: '.deal__navigation .nagination__prev-btn',
